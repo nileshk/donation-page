b444b6c4f50b31cf3000016390741bca7d80f94a
Nilesh Kapadia <code@nileshk.com>
2017-04-16 00:23:08 -0400

    More detailed PushBullet body

--- src/main/java/com/nileshk/PushBulletService.java
+++ src/main/java/com/nileshk/PushBulletService.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
+import static org.apache.commons.lang3.StringUtils.trimToEmpty;
 
 @Service
 public class PushBulletService implements PaymentPostProcessor {
@@ -33,18 +34,33 @@ public class PushBulletService implements PaymentPostProcessor {
 	@Override
 	@Async
 	public void postProcessPayment(Map<String, Object> map, Charge charge, Donation donation) {
-		push(donation);
+		push(donation, "Stripe", charge.getId());
 	}
 
 	@Async
 	@Override
 	public void afterPaypalPayment(Payment payment, Donation donation) {
-		push(donation);
+		push(donation, "PayPal", payment.getId());
 	}
 
-	private void push(Donation donation) {
+	private void push(Donation donation, String paymentType, String id) {
 		String title = String.format("Donation: %s from %s to %s", donation.getAmountString(), donation.getName(), displayName);
-		String body = String.format("Donation: %s from %s to %s", donation.getAmountString(), donation.getName(), siteTitle());
+		StringBuilder sb = new StringBuilder();
+		sb.append(String.format("Donation: %s from %s to %s", donation.getAmountString(), donation.getName(), siteTitle())).append("\n")
+				.append("Name: ").append(trimToEmpty(donation.getName())).append("\n")
+				.append("Amount: ").append(trimToEmpty(donation.getAmountString())).append("\n")
+				.append("Email: ").append(trimToEmpty(donation.getEmail())).append("\n")
+				.append("Address1: ").append(trimToEmpty(donation.getAddress1())).append("\n")
+				.append("Address2: ").append(trimToEmpty(donation.getAddress2())).append("\n")
+				.append("City: ").append(trimToEmpty(donation.getCity())).append("\n")
+				.append("State: ").append(trimToEmpty(donation.getState())).append("\n")
+				.append("Zip: ").append(trimToEmpty(donation.getZip())).append("\n")
+				.append("Country: ").append(trimToEmpty(donation.getCountry())).append("\n")
+				.append("Occupation: ").append(trimToEmpty(donation.getOccupation())).append("\n")
+				.append("Purpose: ").append(trimToEmpty(donation.getPurpose())).append("\n")
+				.append(paymentType).append(" ID: ").append(trimToEmpty(id)).append("\n")
+				.append("Date: ").append(trimToEmpty(donation.getPaymentDate().toString())).append("\n");
+		String body = sb.toString();
 		if (apiTokens != null && apiTokens.size() > 0) {
 			for (String apiToken : apiTokens) {
 				Pushbullet pushbullet = new Pushbullet(apiToken);
